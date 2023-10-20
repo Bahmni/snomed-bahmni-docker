4a75f42903b65adc7ad292919a79c5807080ff0e
Siva Reddy <sivareddy@outlook.com>
2023-10-20 19:40:55 +0530

    BS-248 | Changes for Snowstorm Lite Docker Image and Profile (#11)
    * BS-248 | Changes for SNOWSTORM LITE Docker Image
    
    * BS-248 | Updating docker repository URL for SNOWSTORM LITE
    
    * BS-248 | Adding new profiles for SNOWSTORM LITE
    
    * BS-248 | Adding health check for snowstorm lite service
    
    * BS-248 | Loading RF2 file via service

---
+++ snomed-standard-with-snowstorm-lite/load-data/load-data.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+snowstorm_lite_url="http://snowstorm-lite:8080/fhir"
+
+# Wait for the Snowstorm Lite Server to start
+while true; do
+  http_status_code=$(curl -s -o /dev/null -w "%{http_code}" $snowstorm_lite_url)
+
+  if [ "$http_status_code" -eq 200 ]; then
+    echo "Snowstorm Lite Server is up and running!"
+    break
+  else
+    echo "HTTP status code $http_status_code - Not OK, retrying in 5 seconds...";
+    sleep 5
+  fi
+done
+
+# Make a POST API call to load data
+curl -v -u admin:<UPDATE_PASSWORD_HERE> --form file='@/snowstorm-data/snomed-data.zip' --form version-uri="http://snomed.info/sct/900000000000207008/version/20230731" http://snowstorm-lite:8080/fhir-admin/load-package
+echo "Data Loaded Successfully"
