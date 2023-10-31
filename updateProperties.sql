6486552824c1c238107d63fc3a68724be2603bfc
Siva Reddy <sivareddy@outlook.com>
2023-10-31 14:40:58 +0530

    Retiring drugs from CIEL

--- updateProperties.sql
+++ updateProperties.sql
@@ -13,7 +13,7 @@ update global_property set property_value='ValueSet/$expand?url={0}{1}&displayLa
 update concept set is_set = 1 where concept_id = (select concept_id from concept_name where name="Procedure Orders" and locale="en" and concept_name_type = "FULLY_SPECIFIED");
 update global_property set property_value='http://bahmni.org/fhir/ValueSet/' where property='ts.fhir.procedure.valueset.urltemplate';
 update global_property set property_value='/openmrs/data/fhir-export-anonymise-config.json' where property='fhir.export.anonymise.config.path';
-update drug set retired = 1, retire_reason="Not needed" where date_created < "2023-01-24 14:53:06";
+update drug set retired = 1, retire_reason="Not needed" where date_created < "2023-10-24 14:53:06";
 select user_id INTO @user_id from users where username = 'registration';
 INSERT INTO user_role (user_id, role) VALUES (@user_id, 'FrontDesk');
 select user_id INTO @user_id_doctor from users where username = 'dr_neha';
